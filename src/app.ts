074c205aa52fbc3a94bca1ae67785182c21e1188
mofangbao <mofangbao@gmail.com>
2024-12-11 18:09:53 +0800

    feat:change app name

--- src/app.ts
+++ src/app.ts
@@ -1,53 +1,46 @@
 import { apiPlugin, cogViewPlugin, config, defaultPlugin, getLogger, GlobalEventNames, PPAgent } from "ppagent";
+import samplePlugin from "./plugin.js";
 
-const logger = getLogger('app');
+const logger = getLogger("app");
 
 const starter = async () => {
-    logger.debug('app starting');
+    logger.debug("app starting");
     const chat = new PPAgent({
-        name: 'sample-app',
+        name: "sample",
         agentServiceOptions: {
             models: {
-                bots: [
-                    
-                    
-                ],
-                sources: [
-                ],
-                skills: [
-                    
-                ],
-                agents: [
-                    
-                ],
+                bots: [],
+                sources: [],
+                skills: [],
+                agents: [],
             },
         },
         offline: config.offline,
         taskServiceOptions: {
-            tasks: [
-            ],
+            tasks: [],
         },
     });
     chat.use(defaultPlugin);
     chat.use(apiPlugin);
     chat.use(cogViewPlugin);
+    chat.use(samplePlugin);
     await chat.start();
-    logger.debug('app started');
+    logger.debug("app started");
     return chat;
 };
 
 const onRestart = async (chater: PPAgent) => {
-    logger.warn('app restarting,stop old instance...');
+    logger.warn("app restarting,stop old instance...");
     await chater.stop();
     setTimeout(async () => {
-        logger.warn('old instance stopped,start new instance...');
+        logger.warn("old instance stopped,start new instance...");
         chater.globalEvent.emit(GlobalEventNames.APP_RESTART);
-        chater.off('restart', onRestart);
+        chater.off("restart", onRestart);
         chater = await starter();
-        logger.warn('new instance started');
-        chater.globalEvent.on('restart', () => onRestart(chater));
+        logger.warn("new instance started");
+        chater.globalEvent.on("restart", () => onRestart(chater));
     }, 500);
 };
 
 const chater = await starter();
-chater.globalEvent.on('restart', () => onRestart(chater));
+chater.globalEvent.on("restart", () => onRestart(chater));
