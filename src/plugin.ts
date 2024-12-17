157e71fc64ac2af51985f2cd9d8a152d4937b5fc
mofangbao <baoweiwei@mlogcn.com>
2024-12-17 21:46:47 +0800

    feat:增加示例属性

--- src/plugin.ts
+++ src/plugin.ts
@@ -2,11 +2,31 @@ import { getLogger, IPPAgentPlugin } from "ppagent";
 
 const logger = getLogger("sample-plugin");
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const plugin: IPPAgentPlugin = (app) => {
+const plugin: IPPAgentPlugin = (app, options) => {
+    if (options.demo) {
+        logger.info(options.demo);
+    }
     return {
         name: "sample-plugin",
         desc: "一个示例插件",
         needOnline: false,
+        schema: {
+            type: "object",
+            properties: {
+                demo: {
+                    type: "string",
+                    title: "示例属性",
+                    "x-decorator": "FormItem",
+                    "x-component": "Input",
+                    "x-component-props": {
+                        placeholder: "随便输入点什么，插件启动的时候会在控制台输出",
+                    },
+                    "x-decorator-props": {
+                        tooltip: "这是一个示例用的属性",
+                    },
+                },
+            },
+        },
         init: async () => {
             logger.info("sample plugin init");
             return {};
